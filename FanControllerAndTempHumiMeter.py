e8e1dab8a9f068a269c6e7cf8843298fc89aa28b
Jue Jun Ma <jjma2003@gmail.com>
2021-07-13 02:47:32 +0200

    Added Fan Counter adds to counter from thingspeak

--- FanControllerAndTempHumiMeter.py
+++ FanControllerAndTempHumiMeter.py
@@ -4,14 +4,19 @@ import adafruit_dht
 from rpi_lcd import LCD
 import RPi.GPIO as GPIO 
 import requests
+import json
 #setting up the sensor on GPIO4
 dhtDevice = adafruit_dht.DHT11(board.D4)
 lcd = LCD()
-url= "https://api.thingspeak.com/update"
-# Dictionary with the wire key and dummy in
-queries = {"api_key": "RJM2YTZGVD6YGHZH", "field1": None, "field2": None}
+writeUrl= "https://api.thingspeak.com/update"
+writeQueries = {"api_key": "RJM2YTZGVD6YGHZH", "field1": None, "field2": None, "field3": None}
+readUrl= "https://api.thingspeak.com/channels/1442136/feeds.json"
+readQueries = {"api_key": "IK2UFZMKC5QQ9Y6E", "results": 1}
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(13,GPIO.IN)
+FanCounter = 0
+TempThreshold = 0
 def Main():
     try:
         while True:
@@ -26,7 +31,7 @@ def Main():
                     lcd.text(f"Temp = {temperature:0.1f}C", 1)
                     lcd.text(f"Humi = {humidity}%", 2)
                     # Calling the thingspeak function and giving it the temperature and humidity
-                    Thingspeak(temperature, humidity)
+                    ThingspeakWrite(temperature, humidity)
                     Fan(temperature)
                     time.sleep(2.0)
             except RuntimeError:
@@ -37,17 +42,26 @@ def Main():
         lcd.clear()
         GPIO.output(17, False)
 
-def Thingspeak(temp, humi):
-    # Assignes the right fields with temperature and humidity
-    queries["field1"] = temp
-    queries["field2"] = humi
+def ThingspeakWrite(temp, humi):
+    # Assignes the right fields with temperature, humidity and the amount of time the fan has turned on
+    writeQueries["field1"] = temp
+    writeQueries["field2"] = humi
+    writeQueries["field3"] = FanCounter
     # Sends the temperature and humidity data to thingspeak
-    requests.get(url, params=queries)
+    requests.get(writeUrl, params=writeQueries)
+def ThingspeakRead():
+    global FanCounter
+    data = requests.get(readUrl, params=readQueries).json()
+    FanCounter = int(data["feeds"][0]["field3"])
 
 def Fan(temp):
-    if temp > 27:
+    global FanCounter
+    if temp > TempThreshold and GPIO.input(17) == False:
         GPIO.output(17, True)
-    else:
+        FanCounter += 1
+        print(FanCounter)
+    elif temp < TempThreshold:
         GPIO.output(17, False)
-    
+
+ThingspeakRead()
 Main()
